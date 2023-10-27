b004f148562fc2aa65f82b7fba61964cd6904c63
Justine Zapansa <39612017+zapansa@users.noreply.github.com>
2023-10-27 06:10:39 -0700

    finished * :)

--- prob01/main.cc
+++ prob01/main.cc
@@ -1,65 +1,71 @@
-// Please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
-//
-// Lab 9-1
-// If it is a pair programming lab please specify partner below.
-// Partner: @peteranteater
+/*
+    C++ CSUF Exercises @ Christian S.
+
+    Date: October 27, 2023
+    Author: Justine Zapansa
+*/
 
 #include <iostream>
-
 #include "car.h"
 
 int main() {
-  // =================== YOUR CODE HERE ===================
-  // 1. Create a Car object called `c1` using the default
-  //    constructor.
-  //    Call its Print member function.
-  // ======================================================
+
+  /* 
+    Create a Car object called `c1` using the default constructor.
+  */
+  Car c1 = Car(); 
+  c1.Print();
   std::cout << "\n";
 
-  // =================== YOUR CODE HERE ===================
-  // 2. Create a `VehicleId` object with the following info:
-  //    Model: Honda, ID: 3, License plate: 7B319X4
-  //    Create a `Car` object `c2` using the constructor that
-  //    accepts a `VehicleId` and  pass in the `VehicleId`
-  //    object you just made.
-  //    Call its Print member function.
-  // ======================================================
+  /* 
+    Create a `VehicleId` object with the following info:
+    Model: Honda, ID: 3, License plate: 7B319X4
+    Create a `Car` object `c2` using the constructor that
+    accepts a `VehicleId` and  pass in the `VehicleId`
+    object you just made.
+    Call its Print member function. 
+  */
+  VehicleId vid2 = VehicleId("Honda", 3, "7B319X4");
+  Car c2 = Car(vid2);
+  c2.Print();
   std::cout << "\n";
 
-  // =================== YOUR CODE HERE ===================
-  // 3. Create a `Date` object with the following info:
-  //    Day: 4, Month: 11, Year: 2018
-  //    Create a `Car` object `c3` using the constructor that
-  //    accepts a `Date` object and pass in the `Date` object
-  //    you just made.
-  //    Call its Print member function.
-  // ======================================================
+  /* 
+    Create a `Date` object with the following info:
+    Day: 4, Month: 11, Year: 2018
+    Create a `Car` object `c3` using the constructor that accepts 
+    a `Date` object and pass in the `Date` objectyou just made.
+    Call its Print member function.
+  */
+  Date date3 = Date(4, 11, 2018);
+  Car c3 = Car(date3);
+  c3.Print();
   std::cout << "\n";
 
-  // =================== YOUR CODE HERE ===================
-  // 4. Create a `Car` object `c4` using the constructor that
-  //    accepts a `VehicleId` and `Date` object and pass in
-  //    the `VehicleId` and `Date` objects you created in
-  //    steps 2 and 3 above.
-  //    Call its Print member function.
-  // ======================================================
+  /* 
+    Create a `Car` object `c4` using the constructor that
+    accepts a `VehicleId` and `Date` object and pass in
+    the `VehicleId` and `Date` objects you created in steps 2 and 3 above.
+    Call its Print member function.
+  */
+  Car c4 = Car(vid2, date3);
+  c4.Print();
+  std::cout << "\n";
 
-  // 5. Create an instance of `VehicleId` using the default constructor.
+  /* Create an instance of `VehicleId` using the default constructor. */
+  VehicleId ins_vid = VehicleId();
 
-  // 6. Create an instance of `Date` using the default constructor.
+  /* Create an instance of `Date` using the default constructor. */
+  Date ins_date = Date();
 
-  // 7. Call the `SetId` member function on `c4` and pass
-  //    in the VehicleId you just created.
+  /* Call the `SetId` member function on `c4` and pass in the VehicleId you just created. */
+  c4.setId(ins_vid);
 
-  // 8. Call the `SetReleaseDate` member function on `c4`
-  //    and pass in the VehicleId you just created.
+  /* Call the `SetReleaseDate` member function on `c4`and pass in the VehicleId you just created. */
+  c4.setDate(ins_date);
 
-  // 9. Finally, call the print member function for `c4`.
+  /* Finally, call the print member function for `c4`. */
+  c4.Print(); 
 
   return 0;
 }
